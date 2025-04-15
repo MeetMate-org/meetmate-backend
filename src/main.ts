1e8b9ca069b899b4570ed4cede0ecd61e4eee513
yuriy-kulakovskyi <yurakulak42@gmail.com>
2025-04-15 21:40:11 +0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import * as path from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      process.env.URL_DEV
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('MeetMate API')

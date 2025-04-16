5f20db7e16335d9b3b3686f6c118a6485524de52
yuriy-kulakovskyi <yurakulak42@gmail.com>
2025-04-16 22:45:54 +0300

    feat: returning token during otp resend

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -159,7 +159,15 @@ if (!jwtSecret) {
 
     await this.transporter.sendMail(mailOptions);
 
-    return { message: 'New OTP sent to your email' };
+    // generate JWT token
+    const jwtSecret = process.env.JWT_SECRET;
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET is not defined in the environment variables');
+    }
+
+    const token = jwt.sign({ userId: user._id }, jwtSecret, { expiresIn: '1h' });
+
+    return { message: 'New OTP sent to your email', token: token };
   }
 
   async getUserById(id: string) {
